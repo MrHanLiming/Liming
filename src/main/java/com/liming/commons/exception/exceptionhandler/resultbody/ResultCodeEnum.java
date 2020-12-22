0ddda8b2f998efa3d5a43924f7318c426cd3cc3d
hanliming <limingdi1@>
2020-12-22 09:40:04 +0800

    修改提示信息

--- src/main/java/com/liming/commons/exception/exceptionhandler/resultbody/ResultCodeEnum.java
+++ src/main/java/com/liming/commons/exception/exceptionhandler/resultbody/ResultCodeEnum.java
@@ -7,7 +7,7 @@ public enum ResultCodeEnum {
     NOTACCESS("403","禁止访问"),
     NOTFIND("404","未找到"),
     HTTPIERROR("400","错误的请求"),
-    LOGINERROR("-999","未登录认证");
+    LOGINERROR("-999","请求地址不存在或未登录认证");
 
     private String retCode;
     private String retMsg;
